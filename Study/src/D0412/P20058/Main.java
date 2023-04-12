a7baead932a5d3685a02f878488502d02b555ed3
조태규 <hey03312@gmail.com>
2023-04-13 03:29:30 +0900

    0413 morning

--- Study/src/D0412/P20058/Main.java
+++ Study/src/D0412/P20058/Main.java
@@ -4,9 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, Q, len, L[], A[][];
+	static int N, Q, sum, range, L[];
 	static int[] dx = { 0, 1, 0, -1 };
 	static int[] dy = { 1, 0, -1, 0 };
+	static boolean visited[][];
 
 	public static void main(String[] args) throws Exception {
 
@@ -16,56 +17,124 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
 
-		len = (int) Math.pow(2, N);
-		A = new int[len][len];
+		N = (int) Math.pow(2, N);
+		int[][] A = new int[N][N];
 		L = new int[Q];
 
-		for (int i = 0; i < len; i++) {
+		for (int x = 0; x < N; x++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < len; j++)
-				A[i][j] = Integer.parseInt(st.nextToken());
+			for (int y = 0; y < N; y++)
+				A[x][y] = Integer.parseInt(st.nextToken());
 		}
-
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < Q; i++) {
-			st = new StringTokenizer(br.readLine());
 			L[i] = Integer.parseInt(st.nextToken());
 		}
-
-		fireStorm();
-
+		int[][] ans = fireStorm(A);
+		getAns(ans);
 	}
 
-	private static void fireStorm() {
-		for (int i = 0; i < Q; i++) {
-			rotate(L[i]);
-			crashIce();
-		}
-	}
+	private static void getAns(int[][] map) {
+		visited = new boolean[N][N];
 
-	private static int[][] rotate(int n) {
-		int size = (int) Math.pow(2, n);
-		int[][] temp = new int[len][len];
-
-		for (int i = 0; i < N; i += size) {
-			for (int j = 0; j < N; j += size) {
-				while (size > 1) {
-
-					for (int x = i; x < i + size; x++) {
-						for (int y = i; y < j + size; y++) {
-							temp[x][y] = A[][]
-						}
-					}
-
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				if (!visited[x][y] && map[x][y] > 0) {
+					visited[x][y] = true;
+					sum += map[x][y];
+					bfs(map, x, y);
 				}
-
 			}
 		}
-
-		return temp;
+		System.out.println(sum);
+		System.out.println(range);
 	}
 
-	private static void crashIce() {
+	private static void bfs(int[][] map, int x, int y) {
+		Queue<int[]> q = new ArrayDeque<int[]>();
 
+		q.add(new int[] { x, y });
+		int cnt = 1;
+		while (!q.isEmpty()) {
+			int[] dot = q.poll();
+
+			int nx, ny;
+			for (int d = 0; d < 4; d++) {
+				nx = dot[0] + dx[d];
+				ny = dot[1] + dy[d];
+
+				if (nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny])
+					continue;
+				if (map[nx][ny] == 0)
+					continue;
+
+				q.add(new int[] { nx, ny });
+				visited[nx][ny] = true;
+				sum += map[nx][ny];
+				cnt++;
+			}
+		}
+		range = Math.max(range, cnt);
+	}
+
+	private static int[][] fireStorm(int[][] map) {
+		int[][] tmp1, tmp2;
+		for (int i = 0; i < Q; i++) {
+			tmp1 = new int[N][N];
+			rotate(map, tmp1, L[i]);
+
+			// 회전한 맵
+			tmp2 = new int[N][N];
+			for (int x = 0; x < N; x++) {
+				for (int y = 0; y < N; y++) {
+					tmp2[x][y] = tmp1[x][y];
+				}
+			}
+			crashIce(tmp1, tmp2);
+			// 얼음 파괴된 맵
+			map = tmp2;
+		}
+		return map;
+
+	}
+
+	private static void rotate(int[][] map, int[][] temp, int n) {
+		int size = (int) Math.pow(2, n);
+		for (int x = 0; x < N; x += size) {
+			for (int y = 0; y < N; y += size) {
+
+				for (int i = 0; i < size; i++) {
+					for (int j = 0; j < size; j++) {
+						temp[x + i][y + j] = map[x + size - j - 1][y + i];
+					}
+				}
+			}
+		}
+	}
+
+	private static void crashIce(int[][] map, int[][] temp) {
+
+		for (int x = 0; x < N; x++) {
+			for (int y = 0; y < N; y++) {
+				if (map[x][y] == 0) {
+					continue;
+				}
+
+				int nx, ny;
+				int iceSum = 0;
+				for (int d = 0; d < 4; d++) {
+					nx = x + dx[d];
+					ny = y + dy[d];
+
+					if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+						continue;
+					if (map[nx][ny] > 0)
+						iceSum++;
+				}
+				if (iceSum < 3)
+					temp[x][y]--;
+			}
+		}
 	}
 
 }
